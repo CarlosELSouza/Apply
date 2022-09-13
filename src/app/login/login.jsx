e9e22cb6c509f08e7cb29e8ffeeb2fcdd3baee64
Carlos Eduardo Souza <88462882+CarlosELSouza@users.noreply.github.com>
2022-09-12 21:22:36 -0300

    Trying to hidden icons

--- src/app/login/login.jsx
+++ src/app/login/login.jsx
@@ -7,11 +7,12 @@ function login(){
             <div className="card-login">
                 <h1>APPLY</h1>
                 <div className="textfield">
-                    <i className="fa-solid fa-user icon-login"></i>
+                    <span className="fa-solid fa-user icon-login"/>
                     <input type="text" name="usuario" placeholder="Usuário" />
                 </div>
                 <div className="textfield">
-                    <input type="password" name="senha" placeholder="Senha"/>
+                <input type="password" name="senha" placeholder="Senha"/>
+                <span className="fa-sharp fa-solid fa-key icon-login"/>
                 </div>
                 <button className="btn-login">Login</button>
             </div>
